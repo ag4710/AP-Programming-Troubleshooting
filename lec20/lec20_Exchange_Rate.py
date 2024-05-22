4b945316d43c17e9a757f5480494d9ad5e37283b
ag4710 <leun4710@gmail.com>
2024-05-22 11:12:45 +0900

    add graph code

--- lec20/lec20_Exchange_Rate.py
+++ lec20/lec20_Exchange_Rate.py
@@ -1,5 +1,13 @@
 # Read KRW-USD rates
-# We have data from 1993 to 2009
+# 환율변화 그래프
+import cs1media
+# 그래츠 배경생성 기준값
+x_step = 5
+min_y = 700
+max_y = 2000
+y_step = 0.5
+
+# We have data from 1994 to 2009
 years = range(1994, 2010)
 
 # read one year and return list
@@ -32,9 +40,42 @@ def find_minmax(yr):
         minmax[month] = minr, maxr
     return minmax
 
+
+def main():
+    w = len(years) * 12 * x_step
+    h = int((max_y - min_y) * y_step)
+    p = cs1media.create_picture(w, h, cs1media.Color.white)
+    # vertical line for every january
+    for  yr in years:
+        x = (yr - years[0]) * 12 * x_step
+        for y in range(h):
+            p.set(x, y, cs1media.Color.gray)
+    
+    # horizontal line per 100 Won
+    for won in range(min_y, max_y, 100):
+        y = int((won - min_y) * y_step)
+        for x in range(w):
+            p.set(x, y, cs1media.Color.gray)
+    
+    # plot exchange rate
+    for yr in years:
+        minmax = find_minmax(yr)
+        for m in range(12):
+            x = ((yr - years[0]) * 12 + m) * x_step
+            y1 = int((minmax[m][0] - min_y) * y_step)
+            y2 = int((minmax[m][1] - min_y) * y_step)
+            for y in range(h - y2, h -y1):
+                p.set(x, y, cs1media.Color.red)
+                # p.set(x+1, y, cs1media.Color.red)
+                # p.set(x+2, y, cs1media.Color.red)
+    p.show()
+    p.save_as("lec20/krw.png")
+
 for yr in years:
     print("%4d: " % yr, end =" ")
     minmax = find_minmax(yr)
     for m in range(12):
         print("%4d/%-4d" % minmax[m], end = " ")
     print()
+
+main()
